a2dc411a78c43e2b1746a4bd6b61f434e2a09394
Jason Hewitt <jason.hewitt@skynet.be>
2020-10-01 19:51:53 +0200

    Added debug reading the battery voltage

--- iScore.ino
+++ iScore.ino
@@ -124,7 +124,7 @@ Personal Score Board II
 #define LBL_TO_START_PRESS "Press to start"
 #define LBL_BATTERY "Bat: "
 
-#define LBL_WHOSERVES "Who serves"
+#define LBL_WHOSERVES "Who serves?"
 #define LBL_RESTART "Restart "
 
 #define LBL_SEPARATOR ":"
@@ -386,10 +386,14 @@ void loop()
     updateDisplay();
   }
 
-  delay(50);
+  delay(5000);
 
   // any changes to the model will have been handled
   modelChanged = false;
+
+  // Display current battery voltage
+  float voltage = getBattVoltage();
+  printDebug(voltage);
 
   //  Serial.print("Loop Time");
   //  Serial.print(millis() - time);
@@ -1809,8 +1813,68 @@ void printDebug(char *buffer)
 // prints debug info
 void printDebug(int number)
 {
-
   display.setFont(tiny_6ptFontInfo);
-  display.setCursor(10, 0);
+  display.setCursor(20, 0);
   display.print(number);
+}
+
+// prints debug info
+void printDebug(float number)
+{
+  display.setFont(liberationSans_10ptFontInfo);
+  display.setCursor(20, 0);
+  display.print(number);
+}
+
+// Battery
+// This function gets the battery VCC internally, you can checkout this link
+// if you want to know more about how:
+// http://atmel.force.com/support/articles/en_US/FAQ/ADC-example
+float getVCC()
+{
+  SYSCTRL->VREF.reg |= SYSCTRL_VREF_BGOUTEN;
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  ADC->SAMPCTRL.bit.SAMPLEN = 0x1;
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  ADC->INPUTCTRL.bit.MUXPOS = 0x19; // Internal bandgap input
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  ADC->CTRLA.bit.ENABLE = 0x01; // Enable ADC
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  ADC->SWTRIG.bit.START = 1;   // Start conversion
+  ADC->INTFLAG.bit.RESRDY = 1; // Clear the Data Ready flag
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  ADC->SWTRIG.bit.START = 1; // Start the conversion again to throw out first value
+  while (ADC->INTFLAG.bit.RESRDY == 0)
+    ; // Waiting for conversion to complete
+  uint32_t valueRead = ADC->RESULT.reg;
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  ADC->CTRLA.bit.ENABLE = 0x00; // Disable ADC
+  while (ADC->STATUS.bit.SYNCBUSY == 1)
+    ;
+  SYSCTRL->VREF.reg &= ~SYSCTRL_VREF_BGOUTEN;
+
+  float vcc = (1.1 * 1023.0) / valueRead;
+  return vcc;
+}
+
+// Calculate the battery voltage
+float getBattVoltage(void)
+{
+  const int VBATTpin = A4;
+  float VCC = getVCC();
+
+  // Use resistor division and math to get the voltage
+  float resistorDiv = 0.5;
+  float ADCres = 1023.0;
+  float battVoltageReading = analogRead(VBATTpin);
+  battVoltageReading = analogRead(VBATTpin); // Throw out first value
+  float battVoltage = VCC * battVoltageReading / ADCres / resistorDiv;
+
+  return battVoltage;
 }
